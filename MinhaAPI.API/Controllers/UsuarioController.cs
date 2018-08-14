b068dcb500197af1a1c82bdb5a913af9345027c2
ferranderson <anderson.ferreira@prestadores.amil.com.br>
2018-08-14 11:47:51 -0300

    Primeiro commit

---
+++ MinhaAPI.API/Controllers/UsuarioController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MinhaAPI.Domain.Entidades;
+
+namespace MinhaAPI.API.Controllers
+{
+   
+    public class UsuarioController : ApiController
+    {
+        // GET api/<controller>
+        
+        [HttpGet]
+        public IEnumerable<Usuario> Buscar()
+        {
+            var lista = new List<Usuario>();
+            lista.Add(new Usuario()
+            {
+                UsuarioId = 1,
+                Nome = "Anderson",
+                Senha = "123456"
+            });
+            lista.Add(new Usuario()
+            {
+                UsuarioId = 2,
+                Nome = "Gisele"
+            });
+            lista.Add(new Usuario()
+            {
+                UsuarioId = 3,
+                Nome = "Eduardo"
+            });
+            lista.Add(new Usuario()
+            {
+                UsuarioId = 4,
+                Nome = "Eloisia"
+            });
+            lista.Add(new Usuario()
+            {
+                UsuarioId = 5,
+                Nome = "Antonio"
+            });
+
+            return lista;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MinhaAPI.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MinhaAPI.API.WebApiApplication" Language="C#" %>
